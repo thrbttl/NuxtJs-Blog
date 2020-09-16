569ca68b23371768344193c364b74ec120541a87
Tahir Battal <63402793+thrbttl@users.noreply.github.com>
2020-09-16 15:52:28 +0300

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: "bilogmilog",
+    title: "NuxtJs Blog",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
