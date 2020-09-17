94ca4c6f93a06f63d2e6cd1fb38f95acde3e3c38
Tahir Battal <63402793+thrbttl@users.noreply.github.com>
2020-09-17 22:23:18 +0300

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,10 +11,10 @@ export default {
       {
         hid: "description",
         name: "description",
-        content: "Baştan sona NuxtJs (VueJs) ile geliştirilmiş blog sitesi."
+        content: "Baştan sona NuxtJs (VueJs) ile geliştirilmiş blog sitesi. (Tahir Battal tarafından geliştirildi.)"
       },
       { name: "robots", content: "index, follow" },
-      {name:"keywords", content:"JavaScript, NuxtJs, VueJs, blog, expressJs, node.js"},
+      {name:"keywords", content:"JavaScript, NuxtJs, VueJs, blog, expressJs, node.js, Tahir Battal"},
       {name:"author", content:"Tahir Battal"}
     ],
     link: [
